aff43887c6e42032c4ff475e3b1009af76d94dcd
Tilt <lesma_chan@pm.me>
2025-01-15 09:54:57 -0300

    kiev: Init vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+# vendorsetup.sh
+
+# Device-specific repositories
+git clone https://github.com/Project-Flare-Devices/android_device_motorola_kiev device/motorola/kiev
+git clone https://github.com/Project-Flare-Devices/android_device_motorola_sm7250-common device/motorola/sm7250-common
+
+# Proprietary vendor repositories
+git clone https://github.com/Project-Flare-Devices/proprietary_vendor_motorola_kiev vendor/motorola/kiev
+git clone https://github.com/Project-Flare-Devices/proprietary_vendor_motorola_sm7250-common vendor/motorola/sm7250-common
+
+# Kernel repository
+git clone https://github.com/Project-Flare-Devices/android_kernel_motorola_sm7250 kernel/motorola/sm7250
+
+# Hardware repository
+git clone https://github.com/Project-Flare-Devices/android_hardware_motorola hardware/motorola
