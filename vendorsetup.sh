6f879054c6f0e276fd33e6b9e1fa218d3fc6f698
tilt-rgb <lesma_chan@pm.me>
2025-02-20 19:40:43 +0000

    kiev: vendorsetup: Track more repos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,11 @@ git clone https://github.com/Project-Flare-Devices/android_device_motorola_kiev 
 git clone https://github.com/Project-Flare-Devices/android_device_motorola_sm7250-common device/motorola/sm7250-common
 
 # Proprietary vendor repositories
-git clone https://github.com/Project-Flare-Devices/proprietary_vendor_motorola_kiev vendor/motorola/kiev
+git clone https://gitlab.com/crdroidandroid/proprietary_vendor_motorola_kiev vendor/motorola/kiev
 git clone https://github.com/Project-Flare-Devices/proprietary_vendor_motorola_sm7250-common vendor/motorola/sm7250-common
+
+# Keys
+git clone https://github.com/Lavender-Gang/Keys vendor/lineage-priv/keys
 
 # Kernel repository
 git clone https://github.com/Project-Flare-Devices/android_kernel_motorola_sm7250 kernel/motorola/sm7250
